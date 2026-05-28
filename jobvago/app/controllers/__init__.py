dc4a6fc37e3335c2aef1bebb4dd7c9b73cfef8f2
985shen <77ramble_lug@icloud.com>
2026-05-29 06:25:02 +0800

    Delete jobvago directory

--- jobvago/app/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.controllers.auth_controller import auth_bp
-from app.controllers.career_controller import career_bp
-from app.controllers.dashboard_controller import dashboard_bp
-from app.controllers.resume_controller import resume_bp
-
-__all__ = ['auth_bp', 'career_bp', 'dashboard_bp', 'resume_bp']
--- jobvago/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User, FavoriteCourse, ResumeAnalysis
-from app.models.career import Industry, Course, DashboardStats
-
-__all__ = ['User', 'FavoriteCourse', 'ResumeAnalysis', 'Industry', 'Course', 'DashboardStats']
